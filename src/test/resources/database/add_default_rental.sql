c777af9ef42fbf0ebd08ccfc398fa5562e8de98b
Kamil Szerszen <szerszenk1997@gmail.com>
2025-09-11 11:15:06 +0200

    Add test for Rental: Service, Repository, Controller. Evrything ok, all test passed

---
+++ src/test/resources/database/add_default_rental.sql
@@ -0,0 +1,7 @@
+insert into rentals (id, rental_date, return_date, actual_return_date, car_id, user_id)
+values (1,
+        '2025-09-11 10:00:00',
+        '2025-09-15 18:00:00',
+        NULL,
+        1,
+        1);
